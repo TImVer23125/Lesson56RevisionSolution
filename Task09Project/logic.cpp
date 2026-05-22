7b2507e9cc0421f50eb656cf93f154b52a8bdcb8
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-05-22 19:36:21 +0300

    Solve Task09

--- Task09Project/logic.cpp
+++ Task09Project/logic.cpp
@@ -3,3 +3,20 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму цифр заданного числа, кратных 5
+#include "logic.h"
+
+int recursion(int number) {
+	if (number == 0) {
+		return 0;
+	}
+
+	int digit = number % 10;
+
+	return recursion(number / 10) + (digit % 5 == 0 ? digit : 0);
+}
+
+int sum(int number){
+	number = number < 0 ? -number : number;
+
+	return recursion(number);
+}
