95262de90012105167690116858ecd68903ff390
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-05-22 19:20:43 +0300

    Solve Task07

--- Task07Project/logic.cpp
+++ Task07Project/logic.cpp
@@ -4,3 +4,24 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая считает количество нечётных цифр
 // в заданном целом числе.
+
+// f(123456) --> f(12345) + 0
+// f(12345) --> f(1234) + 1 
+
+#include "logic.h"
+
+int recursion(int number) {
+	if (number == 0) {
+		return 0;
+	}
+
+	int digit = number % 10;
+
+	return recursion(number / 10) + (digit % 2 == 1 ? 1 : 0);
+}
+
+int count_of_odd_digits(int number) {
+	number = number < 0 ? -number : number;
+
+	return recursion(number);
+}
