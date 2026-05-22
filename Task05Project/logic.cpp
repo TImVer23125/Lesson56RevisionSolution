84fc8d840db6fc9546ef5841731e765290371d12
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-05-22 18:49:03 +0300

    Solve Task05

--- Task05Project/logic.cpp
+++ Task05Project/logic.cpp
@@ -4,3 +4,19 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая подсчитывает количество цифр 
 // в заданном целом числе.
+
+#include "logic.h"
+
+int recursion(int number) {
+	if (number > -10 && number < 10) {
+		return 1;
+	}
+
+	return recursion(number / 10) + 1;
+}
+
+int count(int number) {
+	number = number < 0 ? -number : number;
+
+	return recursion(number);
+}
